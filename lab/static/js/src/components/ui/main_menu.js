56e9e814240f63c1e52f021f2d1384029ff6d705
ospreyelm <rpmoseley@gmail.com>
2018-02-06 00:26:17 -0500

    menu

--- lab/static/js/src/components/ui/main_menu.js
+++ lab/static/js/src/components/ui/main_menu.js
@@ -71,7 +71,7 @@ define([
 	MainMenuComponent.prototype.toggleMenu = function(state) {
 		var that = this;
 		this.$menuEl.animate({
-			// width: (state?"show":"hide")
+			fontSize: (state?"show":"hide")
 		}, {
 			duration: 1,
 			complete: function() {
