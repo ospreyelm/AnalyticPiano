62ab33ed633d8ac020cd1be19c4028f7925d1c90
ospreyelm <rpmoseley@gmail.com>
2020-08-27 00:13:00 -0400

    groundwork for better horizontal spacing of staff signature, but may be detrimental to app performance and user comparison of different keys

--- lab/static/js/src/components/music/exercise_sheet.js
+++ lab/static/js/src/components/music/exercise_sheet.js
@@ -419,6 +419,7 @@ define([
 
             stave.setRenderer(this.vexRenderer);
             stave.setKeySignature(this.keySignature);
+            // stave.setFirstBarWidth(this.keySignature);
             stave.setNotater(stave_notater);
             stave.setMaxWidth(this.getWidth());
             stave.updatePosition();
@@ -455,6 +456,7 @@ define([
 
             stave.setRenderer(this.vexRenderer);
             stave.setKeySignature(this.keySignature);
+            // stave.setFirstBarWidth(this.keySignature);
             stave.setNoteFactory(new ExerciseNoteFactory({
                 clef: clef,
                 chord: displayChord,
