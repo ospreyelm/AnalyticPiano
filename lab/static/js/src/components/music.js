2a17e7ca17f9290134fe79150959f6ad30384b24
ospreyelm <rpmoseley@gmail.com>
2020-09-05 16:38:47 -0400

    convert indentation to spaces; no other change

--- lab/static/js/src/components/music.js
+++ lab/static/js/src/components/music.js
@@ -1,159 +1,159 @@
 define([
-	'jquery',
-	'lodash',
-	'app/config',
-	'app/components/events',
-	'app/components/component',
-	'app/models/key_signature',
-	'./music/play_sheet'
+    'jquery',
+    'lodash',
+    'app/config',
+    'app/components/events',
+    'app/components/component',
+    'app/models/key_signature',
+    './music/play_sheet'
 ], function(
-	$,
-	_,
-	Config,
-	EVENTS,
-	Component,
-	KeySignature,
-	PlainSheetComponent
+    $,
+    _,
+    Config,
+    EVENTS,
+    Component,
+    KeySignature,
+    PlainSheetComponent
 ) {
-	/**
-	 * This is a map of analysis modes to booleans indicating whether the mode
-	 * is enabled or disabled by default.
-	 * @type {object}
-	 */
-	var ANALYSIS_SETTINGS = Config.get('general.analysisSettings');
-	/**
-	 * This is a map of highlight modes to booleans indicating whether the mode
-	 * is enabled or disabled by default.
-	 * @type {object}
-	 */
-	var HIGHLIGHT_SETTINGS = Config.get('general.highlightSettings');
+    /**
+     * This is a map of analysis modes to booleans indicating whether the mode
+     * is enabled or disabled by default.
+     * @type {object}
+     */
+    var ANALYSIS_SETTINGS = Config.get('general.analysisSettings');
+    /**
+     * This is a map of highlight modes to booleans indicating whether the mode
+     * is enabled or disabled by default.
+     * @type {object}
+     */
+    var HIGHLIGHT_SETTINGS = Config.get('general.highlightSettings');
 
-	var STAFF_DISTRIBUTION = Config.get('general.staffDistribution');
+    var STAFF_DISTRIBUTION = Config.get('general.staffDistribution');
 
-	/**
-	 * Creates an instance of MusicComponent.
-	 *
-	 * This object is responsible for displaying the sheet music and
-	 * handling runtime configuration changes.
-	 *
-	 * @constructor
-	 * @param {object} settings
-	 * @param {Sheet} settings.sheet Required property.
-	 */
-	var MusicComponent = function(settings) {
-		this.settings = settings || {};
-		this.settings.analysisSettings = this.settings.analysisSettings || {};
-		this.settings.highlightSettings = this.settings.highlightSettings || {};
+    /**
+     * Creates an instance of MusicComponent.
+     *
+     * This object is responsible for displaying the sheet music and
+     * handling runtime configuration changes.
+     *
+     * @constructor
+     * @param {object} settings
+     * @param {Sheet} settings.sheet Required property.
+     */
+    var MusicComponent = function(settings) {
+        this.settings = settings || {};
+        this.settings.analysisSettings = this.settings.analysisSettings || {};
+        this.settings.highlightSettings = this.settings.highlightSettings || {};
 
-		/**
-		 * Defines the music element.
-		 * @type {jQuery}
-		 */
-		this.el = $('<div></div>');
-		/**
-		 * Configuration settings for highlighting notes on the sheet music.
-		 * @type {object}
-		 */
-		this.highlightConfig = _.extend({}, HIGHLIGHT_SETTINGS, this.settings.highlightSettings);
-		/**
-		 * Configuration settings for analyzing notes on the sheet music.
-		 * @type {object}
-		 */
-		this.analyzeConfig = _.extend({tempo:false}, ANALYSIS_SETTINGS, this.settings.analysisSettings);
-		/**
-		 * Configuration settings for staff distribution on the sheet music.
-		 * @type {object}
-		 */
-		this.staffDistributionConfig = _.extend({}, STAFF_DISTRIBUTION, this.settings);
+        /**
+         * Defines the music element.
+         * @type {jQuery}
+         */
+        this.el = $('<div></div>');
+        /**
+         * Configuration settings for highlighting notes on the sheet music.
+         * @type {object}
+         */
+        this.highlightConfig = _.extend({}, HIGHLIGHT_SETTINGS, this.settings.highlightSettings);
+        /**
+         * Configuration settings for analyzing notes on the sheet music.
+         * @type {object}
+         */
+        this.analyzeConfig = _.extend({tempo:false}, ANALYSIS_SETTINGS, this.settings.analysisSettings);
+        /**
+         * Configuration settings for staff distribution on the sheet music.
+         * @type {object}
+         */
+        this.staffDistributionConfig = _.extend({}, STAFF_DISTRIBUTION, this.settings);
 
 
-		if(!("sheet" in this.settings)) {
-			throw new Error("missing settings.sheet parameter");
-		}
+        if(!("sheet" in this.settings)) {
+            throw new Error("missing settings.sheet parameter");
+        }
 
-		// this.setComponent("pristine", this.settings.pristine);
-		this.setComponent("sheet", this.settings.sheet);
+        // this.setComponent("pristine", this.settings.pristine);
+        this.setComponent("sheet", this.settings.sheet);
 
-		_.bindAll(this, [
-			'onAnalyzeChange',
-			'onHighlightChange',
-			'onStaffDistributionChange',
-			'onMetronomeChange',
-			'onRedrawRequest'
-		]);
-	};
+        _.bindAll(this, [
+            'onAnalyzeChange',
+            'onHighlightChange',
+            'onStaffDistributionChange',
+            'onMetronomeChange',
+            'onRedrawRequest'
+        ]);
+    };
 
-	MusicComponent.prototype = new Component();
+    MusicComponent.prototype = new Component();
 
-	_.extend(MusicComponent.prototype, {
-		/**
-		 * Initializes the music.
-		 *
-		 * @return undefined
-		 */
-		initComponent: function() {
-			this.initListeners();
-		},
-		/**
-		 * Initializes event listeners.
-		 *
-		 * @return undefined
-		 */
-		initListeners: function() {
-			this.subscribe(EVENTS.BROADCAST.HIGHLIGHT_NOTES, this.onHighlightChange);
-			this.subscribe(EVENTS.BROADCAST.ANALYZE_NOTES, this.onAnalyzeChange);
-			this.subscribe(EVENTS.BROADCAST.ANALYZE_NOTES, this.onStaffDistributionChange); // ?
-			this.subscribe(EVENTS.BROADCAST.METRONOME, this.onMetronomeChange);
-			this.subscribe(EVENTS.BROADCAST.PRISTINE, this.onRedrawRequest);
-		},
-		/**
-		 * Renders the music.
-		 *
-		 * @return this
-		 */
-		render: function() {
-			this.renderSheet();
-			return this;
-		},
-		/**
-		 * Renders the sheet.
-		 *
-		 * @return this
-		 */
-		renderSheet: function() {
-			var sheetComponent = this.getComponent('sheet');
-			sheetComponent.clear();
-			if (sheetComponent.hasOwnProperty('exerciseContext')) {
-				var exercise_midi_nums = sheetComponent.exerciseContext.exerciseChords._items.map(item => Object.keys(item._notes));
-				sheetComponent.render(exercise_midi_nums);
-			} else {
-				sheetComponent.render();
-			}
-			return this;
-		},
-		renderPristine: function() {
-			var sheetComponent = this.getComponent('sheet');
+    _.extend(MusicComponent.prototype, {
+        /**
+         * Initializes the music.
+         *
+         * @return undefined
+         */
+        initComponent: function() {
+            this.initListeners();
+        },
+        /**
+         * Initializes event listeners.
+         *
+         * @return undefined
+         */
+        initListeners: function() {
+            this.subscribe(EVENTS.BROADCAST.HIGHLIGHT_NOTES, this.onHighlightChange);
+            this.subscribe(EVENTS.BROADCAST.ANALYZE_NOTES, this.onAnalyzeChange);
+            this.subscribe(EVENTS.BROADCAST.ANALYZE_NOTES, this.onStaffDistributionChange); // ?
+            this.subscribe(EVENTS.BROADCAST.METRONOME, this.onMetronomeChange);
+            this.subscribe(EVENTS.BROADCAST.PRISTINE, this.onRedrawRequest);
+        },
+        /**
+         * Renders the music.
+         *
+         * @return this
+         */
+        render: function() {
+            this.renderSheet();
+            return this;
+        },
+        /**
+         * Renders the sheet.
+         *
+         * @return this
+         */
+        renderSheet: function() {
+            var sheetComponent = this.getComponent('sheet');
+            sheetComponent.clear();
+            if (sheetComponent.hasOwnProperty('exerciseContext')) {
+                var exercise_midi_nums = sheetComponent.exerciseContext.exerciseChords._items.map(item => Object.keys(item._notes));
+                sheetComponent.render(exercise_midi_nums);
+            } else {
+                sheetComponent.render();
+            }
+            return this;
+        },
+        renderPristine: function() {
+            var sheetComponent = this.getComponent('sheet');
 
-			if (!sheetComponent.hasOwnProperty('exerciseContext')) {
+            if (!sheetComponent.hasOwnProperty('exerciseContext')) {
 
-				/* play view */
-				sheetComponent.chords.clear();
+                /* play view */
+                sheetComponent.chords.clear();
 
-			} else {
+            } else {
 
-				/* exercise view */
-				let scex = sheetComponent.exerciseContext;
+                /* exercise view */
+                let scex = sheetComponent.exerciseContext;
 
-				scex.inputChords.clear();
-				scex.inputChords.goTo(0);
+                scex.inputChords.clear();
+                scex.inputChords.goTo(0);
 
-				let setdef = scex.settings.definition;
+                let setdef = scex.settings.definition;
 
-				var testing = (window.location.href.split(".")[0].slice(-5) == "-beta" ? true : true);
-				if (testing) {
+                var testing = (window.location.href.split(".")[0].slice(-5) == "-beta" ? true : true);
+                if (testing) {
 
-					// The following was used to generate newData as we built this function
-					// let currentData = JSON.stringify(setdef.settings.definition, null, 0);
+                    // The following was used to generate newData as we built this function
+                    // let currentData = JSON.stringify(setdef.settings.definition, null, 0);
                     let newData = {};
 
                     // The AJAX call for Redraw
@@ -170,7 +170,7 @@ define([
                     // success: function (data) {
                     //         newData = data;
                     //     }
-				    // });
+                    // });
 
                     // The AJAX call for Next Exercise
                     $.ajax({
@@ -190,157 +190,157 @@ define([
                             console.log('No next exercise; end of playlist');
                             break;
                         }
-				    });
+                    });
 
-				    if (!newData.length) break;
+                    if (!newData.length) break;
 
-					scex.definition.exercise
-						= scex.definition.parse(newData);
+                    scex.definition.exercise
+                        = scex.definition.parse(newData);
 
-					scex.definition.settings.definition
-						= newData;
+                    scex.definition.settings.definition
+                        = newData;
 
-					scex.settings.definition.exercise
-						= scex.definition.parse(newData);
+                    scex.settings.definition.exercise
+                        = scex.definition.parse(newData);
 
-					scex.settings.definition.settings.definition
-						= newData;
+                    scex.settings.definition.settings.definition
+                        = newData;
 
-					sheetComponent.keySignature
-						= new KeySignature(newData.key, newData.keySignature);
+                    sheetComponent.keySignature
+                        = new KeySignature(newData.key, newData.keySignature);
 
-					sheetComponent.settings.keySignature
-						= new KeySignature(newData.key, newData.keySignature);
+                    sheetComponent.settings.keySignature
+                        = new KeySignature(newData.key, newData.keySignature);
 
-					// DOES NOT HAVE EXPECTED EFFECT
-					this.settings.staffDistribution
-						= newData.staffDistribution;
+                    // DOES NOT HAVE EXPECTED EFFECT
+                    this.settings.staffDistribution
+                        = newData.staffDistribution;
 
-					this.staffDistributionConfig.staffDistribution
-						= newData.staffDistribution;
+                    this.staffDistributionConfig.staffDistribution
+                        = newData.staffDistribution;
 
-					/* similar to updateSettings */
-					// is there a way to do these things once each?
-					Object.assign(this.analyzeConfig, newData.analysis);
-					Object.assign(this.settings.analysisSettings, newData.analysis);
-					Object.assign(this.staffDistributionConfig.analysisSettings, newData.analysis);
+                    /* similar to updateSettings */
+                    // is there a way to do these things once each?
+                    Object.assign(this.analyzeConfig, newData.analysis);
+                    Object.assign(this.settings.analysisSettings, newData.analysis);
+                    Object.assign(this.staffDistributionConfig.analysisSettings, newData.analysis);
 
-					Object.assign(this.highlightConfig, newData.highlight);
-					Object.assign(this.settings.highlightSettings, newData.highlight);
-					Object.assign(this.staffDistributionConfig.highlightSettings, newData.highlight);
-					/* add use of listeners here to update the menu */
+                    Object.assign(this.highlightConfig, newData.highlight);
+                    Object.assign(this.settings.highlightSettings, newData.highlight);
+                    Object.assign(this.staffDistributionConfig.highlightSettings, newData.highlight);
+                    /* add use of listeners here to update the menu */
 
-					scex.inputChords.staffDistribution = newData.staffDistribution;
-					scex.displayChords = scex.createDisplayChords();
-					scex.exerciseChords = scex.createExerciseChords();
+                    scex.inputChords.staffDistribution = newData.staffDistribution;
+                    scex.displayChords = scex.createDisplayChords();
+                    scex.exerciseChords = scex.createExerciseChords();
 
-					this.trigger('change');
-				}
+                    this.trigger('change');
+                }
 
-				// sheetComponent.renderExerciseText(); // not necessary
-				scex.sealed = false;
-				scex.done = false;
-				scex.timer = null;
-				scex.timepoints = [];
+                // sheetComponent.renderExerciseText(); // not necessary
+                scex.sealed = false;
+                scex.done = false;
+                scex.timer = null;
+                scex.timepoints = [];
 
-				window.console.dir('send dummy note');
-				this.broadcast(EVENTS.BROADCAST.NOTE, 'on', 109, 0);
-				this.broadcast(EVENTS.BROADCAST.NOTE, 'off', 109, 0);
+                window.console.dir('send dummy note');
+                this.broadcast(EVENTS.BROADCAST.NOTE, 'on', 109, 0);
+                this.broadcast(EVENTS.BROADCAST.NOTE, 'off', 109, 0);
 
-				scex.state = "ready"; // READY
-			}
+                scex.state = "ready"; // READY
+            }
 
-			sheetComponent.clear();
-			sheetComponent.render();
+            sheetComponent.clear();
+            sheetComponent.render();
 
-			return this;
-		},
-		/**
-		 * Returns the width.
-		 *
-		 * @return {number}
-		 */
-		getWidth: function() {
-			return this.el.width();
-		},
-		/**
-		 * Returns the height.
-		 *
-		 * @return {number}
-		 */
-		getHeight: function() {
-			return this.el.height();
-		},
-		/**
-		 * Handles a change to the highlight settings.
-		 *
-		 * @param {object} settings
-		 * @return undefined
-		 */
-		onHighlightChange: function(settings) {
-			this.updateSettings('highlightConfig', settings);
-			this.trigger('change');
-		},
-		/**
-		 * Handles a change to the analyze settings.
-		 *
-		 * @param {object} settings
-		 * @return undefined
-		 */
-		onAnalyzeChange: function(settings) {
-			this.updateSettings('analyzeConfig', settings);
-			this.trigger('change');
-		},
-		/**
-		 * Handles a change to the staff distribution setting.
-		 *
-		 * @param {object} settings
-		 * @return undefined
-		 */
-		onStaffDistributionChange: function(settings) {
-			this.updateSettings('staffDistributionConfig', settings);
-			this.trigger('change');
-		},
-		/**
-		 * Handles a change to the metronome settings.
-		 *
-		 * @param {object} settings
-		 * @return undefined
-		 */
-		onMetronomeChange: function(metronome) {
-			if(metronome.isPlaying()) {
-				this.analyzeConfig.tempo = metronome.getTempo();
-			} else {
-				this.analyzeConfig.tempo = false;
-			}
-			this.render();
-		},
-		onRedrawRequest: function() {
-			this.renderPristine();
-		},
-		/**
-		 * Updates settings.
-		 *
-		 * @param {string} prop
-		 * @param {object} setting
-		 * @return this
-		 */
-		updateSettings: function(prop, setting) {
-			var mode = _.cloneDeep(this[prop].mode);
-			switch(setting.key) {
-				case "enabled":
-					this[prop].enabled = setting.value;
-					break;
-				case "mode":
-					_.assign(mode, setting.value);
-					this[prop].mode = mode;
-					break;
-				default:
-					throw new Error("Invalid setting key");
-			}
-			return this;
-		},
-	});
+            return this;
+        },
+        /**
+         * Returns the width.
+         *
+         * @return {number}
+         */
+        getWidth: function() {
+            return this.el.width();
+        },
+        /**
+         * Returns the height.
+         *
+         * @return {number}
+         */
+        getHeight: function() {
+            return this.el.height();
+        },
+        /**
+         * Handles a change to the highlight settings.
+         *
+         * @param {object} settings
+         * @return undefined
+         */
+        onHighlightChange: function(settings) {
+            this.updateSettings('highlightConfig', settings);
+            this.trigger('change');
+        },
+        /**
+         * Handles a change to the analyze settings.
+         *
+         * @param {object} settings
+         * @return undefined
+         */
+        onAnalyzeChange: function(settings) {
+            this.updateSettings('analyzeConfig', settings);
+            this.trigger('change');
+        },
+        /**
+         * Handles a change to the staff distribution setting.
+         *
+         * @param {object} settings
+         * @return undefined
+         */
+        onStaffDistributionChange: function(settings) {
+            this.updateSettings('staffDistributionConfig', settings);
+            this.trigger('change');
+        },
+        /**
+         * Handles a change to the metronome settings.
+         *
+         * @param {object} settings
+         * @return undefined
+         */
+        onMetronomeChange: function(metronome) {
+            if(metronome.isPlaying()) {
+                this.analyzeConfig.tempo = metronome.getTempo();
+            } else {
+                this.analyzeConfig.tempo = false;
+            }
+            this.render();
+        },
+        onRedrawRequest: function() {
+            this.renderPristine();
+        },
+        /**
+         * Updates settings.
+         *
+         * @param {string} prop
+         * @param {object} setting
+         * @return this
+         */
+        updateSettings: function(prop, setting) {
+            var mode = _.cloneDeep(this[prop].mode);
+            switch(setting.key) {
+                case "enabled":
+                    this[prop].enabled = setting.value;
+                    break;
+                case "mode":
+                    _.assign(mode, setting.value);
+                    this[prop].mode = mode;
+                    break;
+                default:
+                    throw new Error("Invalid setting key");
+            }
+            return this;
+        },
+    });
 
-	return MusicComponent;
+    return MusicComponent;
 });
