6cbe586c439320675a77c10ff283b5c6cf64c385
ospreyelm <rpmoseley@gmail.com>
2020-10-26 20:56:21 -0400

    From avn

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,10 +1,14 @@
+import json
 from django import forms
 from django.contrib.auth import authenticate, login as django_login, get_user_model
 from django.contrib.auth.views import LogoutView as DjangoLogoutView, LoginView as DjangoLoginView
+from django.core import serializers
+from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 
 from .forms import CustomAuthenticationForm, RegistrationForm, ForgotPasswordForm
+from .models import User
 
 User = get_user_model()
 
@@ -67,3 +71,24 @@ def forgot_password_view(request):
     else:
         form = ForgotPasswordForm()
         return render(request, 'accounts/send_password.html', {'form': form})
+
+
+# The following view function is used to send the keyboard size to the front-end
+# Can be extended to include other preferences as it sends a JSON
+
+def send_keyboard_size(request):
+    if request.is_ajax and request.method == "GET":
+        cur_user = request.user
+        if cur_user.is_anonymous:
+            data_set = {"keyboard_size": 49}
+            json_dump = json.dumps(data_set)
+            return JsonResponse({"instance": json_dump}, status=200)
+
+        keyboard_size = cur_user.keyboard_size
+        print ("keyboard size is ", keyboard_size)
+        data_set = {"keyboard_size": keyboard_size}
+        json_dump = json.dumps(data_set)
+        return JsonResponse({"instance": json_dump}, status=200)
+    else:
+         # some form errors occured.
+        return JsonResponse({"error": "something went wrong"}, status=400)
