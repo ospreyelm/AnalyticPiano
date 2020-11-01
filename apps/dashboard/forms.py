54bdf7fba21858eb0868444c07a6af819a1f8a63
ospreyelm <rpmoseley@gmail.com>
2020-11-01 13:07:09 -0500

    From AVN

--- apps/dashboard/forms.py
+++ apps/dashboard/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from apps.accounts.models import KEYBOARD_CHOICES
 
 User = get_user_model()
 
@@ -10,3 +11,11 @@ class AddSupervisorForm(forms.Form):
 
 class AddSubscriberForm(forms.Form):
     email = forms.EmailField(label='Enter subscriber email:')
+
+class KeyboardForm(forms.Form):
+    keyboard_size = forms.ChoiceField(widget=forms.Select(), choices=KEYBOARD_CHOICES, initial=49)
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user',None)
+        super(KeyboardForm, self).__init__(*args, **kwargs)
+
