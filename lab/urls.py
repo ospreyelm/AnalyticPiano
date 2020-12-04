7d5fd4e319d4977f4514773675faacc6ed879a92
ospreyelm <rpmoseley@gmail.com>
2020-12-04 00:30:37 -0500

    Urls fix
    from pouya-asghari777

--- lab/urls.py
+++ lab/urls.py
@@ -26,21 +26,22 @@ urlpatterns = [
     path('keyboard-size/', send_keyboard_size, name='keyboard-size'),
     path('exercises/keyboard-size/', send_keyboard_size, name='keyboard-size'),
 
-    # Exercises, Playlists and Courses
     # FIXME should be added to a course: ^courses/(?P<course_id>\d+)/exercises/add/$?
     re_path(r'^exercises/add$', AddExerciseView.as_view(), name='add-exercise'),
-    re_path(r'^manage$', ManageView.as_view(), name="manage"),
-    path('exercises/<str:exercise_id>/', ExerciseView.as_view(), name="exercise-view"),
-    path('exercises/<str:group_name>', PlaylistView.as_view(), name="exercise-groups"),
-    path('exercises/<str:group_name>/<int:exercise_num>', PlaylistView.as_view(), name="exercises"),
-    path('courses/<str:course_slug>/', CourseView.as_view(), name="course-view"),
-    re_path(r'definition$', RefreshExerciseDefinition.as_view(),
-            name="refresh-definition"),
-    re_path(r'^exercises$', PlaylistView.as_view()),
 
     # Performance
     re_path(r'exercise-performance$', submit_exercise_performance, name='exercise-performance'),
     re_path(r'playlist-performance$', submit_playlist_performance, name='playlist-performance'),
+
+    # Exercises, Playlists, Courses
+    re_path(r'^manage$', ManageView.as_view(), name="manage"),
+    path('exercises/<str:exercise_id>/', ExerciseView.as_view(), name="exercise-view"),
+    path('exercises/<str:group_name>/<int:exercise_num>', PlaylistView.as_view(), name="exercises"),
+    re_path(r'definition$', RefreshExerciseDefinition.as_view(),
+            name="refresh-definition"),
+    re_path(r'^exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)$', PlaylistView.as_view(), name="exercise-groups"),
+    re_path(r'^exercises$', PlaylistView.as_view()),
+    path('courses/<str:course_slug>/', CourseView.as_view(), name="course-view"),
 
     # API
     re_path(r'^api$', APIView.as_view(), name="api"),
@@ -57,13 +58,14 @@ urlpatterns = [
          playlist_performance_view,
          name='performance-report'),
 
-    # Deprecated
+
+    # DEPRECATED
+    # re_path(r'^courses/(?P<course_id>\d+)/manage$', ManageView.as_view(), name="course-manage"),
+    # re_path(r'^courses/(?P<course_id>\d+)/authcheck$', check_course_authorization, name="course-authorization-check"),
     # re_path(r'^courses/(?P<course_id>\d+)/exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)/(?P<exercise_name>\d+)$',
     #         PlaylistView.as_view(), name="course-exercises"),
     # re_path(r'^courses/(?P<course_id>\d+)/exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)$', PlaylistView.as_view(),
     #         name="course-exercise-groups"),
-    # re_path(r'^courses/(?P<course_id>\d+)/manage$', ManageView.as_view(), name="course-manage"),
-    # re_path(r'^courses/(?P<course_id>\d+)/authcheck$', check_course_authorization, name="course-authorization-check"),
     # re_path(r'^courses/(?P<course_id>\d+)/exercises$', PlaylistView.as_view()),
     # re_path(r'^courses/(?P<course_id>\d+)$', PlayView.as_view(), name="course-index"),
 ]
