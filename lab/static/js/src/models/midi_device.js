d5a6a0b3912f343b588b89aadb87ade26f0b18a8
ospreyelm <rpmoseley@gmail.com>
2021-01-18 13:19:20 -0500

    start to incorporate Tone.js sampler

--- lab/static/js/src/models/midi_device.js
+++ lab/static/js/src/models/midi_device.js
@@ -43,6 +43,47 @@ define([
 
     /* Create Polyphonic Synthesizer */
     var polySynth = new Tone.PolySynth(10, Tone.FMSynth);
+
+    /* Create sampler */
+    // const sampler = new Tone.Sampler({
+    //     urls: {
+    //         "A0": "A0.ogg",
+    //         "C1": "C1.ogg",
+    //         "D#1": "Ds1.ogg",
+    //         "F#1": "Fs1.ogg",
+    //         "A1": "A1.ogg",
+    //         "C2": "C2.ogg",
+    //         "D#2": "Ds2.ogg",
+    //         "F#2": "Fs2.ogg",
+    //         "A2": "A2.ogg",
+    //         "C3": "C3.ogg",
+    //         "D#3": "Ds3.ogg",
+    //         "F#3": "Fs3.ogg",
+    //         "A3": "A3.ogg",
+    //         "C4": "C4.ogg",
+    //         "D#4": "Ds4.ogg",
+    //         "F#4": "Fs4.ogg",
+    //         "A4": "A4.ogg",
+    //         "C5": "C5.ogg",
+    //         "D#5": "Ds5.ogg",
+    //         "F#5": "Fs5.ogg",
+    //         "A5": "A5.ogg",
+    //         "C6": "C6.ogg",
+    //         "D#6": "Ds6.ogg",
+    //         "F#6": "Fs6.ogg",
+    //         "A6": "A6.ogg",
+    //         "C7": "C7.ogg",
+    //         "D#7": "Ds7.ogg",
+    //         "F#7": "Fs7.ogg",
+    //         "A7": "A7.ogg",
+    //         "C8": "C8.ogg"
+    //     },
+    //     release: 1,
+    //     baseUrl: "https://tonejs.github.io/audio/salamander/"
+    // }).toDestination();
+
+
+    /* volume */
     var vol = new Tone.Volume(-6).toMaster();
     polySynth.connect(vol);
 
