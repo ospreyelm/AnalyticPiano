d63eef7e5ce9b977cb87160ce1fc7bc56aa76125
Pouya Asghari <pouyaasghari777@gmail.com>
2021-09-17 19:19:55 +0430

    integrate Sentry

--- harmony/settings/local.py
+++ harmony/settings/local.py
@@ -13,3 +13,20 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+
+        environment=ENVIRONMENT_LOCAL,
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
