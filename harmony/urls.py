02eda039dfa4580e83a4ffbae5302d6b9dd7d19f
ospreyelm <rpmoseley@gmail.com>
2022-06-17 00:06:42 -0400

    correct merge resolution mistake

--- harmony/urls.py
+++ harmony/urls.py
@@ -30,6 +30,9 @@ urlpatterns = [
 
 handler404 = 'harmony.views.error_404'
 
+# FIXME remove after Sentry test was successful
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns += [
     path('sentry-debug/', trigger_error),
