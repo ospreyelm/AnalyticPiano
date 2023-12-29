f2a0ebf38870eea0752ec9a8e22c84377eadc3e0
ospreyelm <rpmoseley@gmail.com>
2023-12-28 23:59:27 -0500

    override bad merge of dashboard/urls.py

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -58,90 +58,44 @@ urlpatterns = [
     path("exercises/", exercises_list_view, name="exercises-list"),
     path("exercises/add/", exercise_add_view, name="add-exercise"),
     path("exercises/<str:exercise_id>/", exercise_edit_view, name="edit-exercise"),
-    path(
-        "exercises/<str:exercise_id>/delete/",
-        exercise_delete_view,
-        name="delete-exercise",
-    ),
+    path("exercises/<str:exercise_id>/delete/", exercise_delete_view, name="delete-exercise"),
     # Playlists
     path("playlists/", playlists_list_view, name="playlists-list"),
     path("playlists/add/", playlist_add_view, name="add-playlist"),
     path("playlists/<str:playlist_id>/", playlist_edit_view, name="edit-playlist"),
-    path(
-        "playlists/<str:playlist_id>/delete/",
-        playlist_delete_view,
-        name="delete-playlist",
-    ),
+    path("playlists/<str:playlist_id>/delete/", playlist_delete_view, name="delete-playlist"),
     # Courses
     path("courses/", courses_list_view, name="courses-list"),
     path("courses/add/", course_add_view, name="add-course"),
     path("courses/<str:course_id>/", course_edit_view, name="edit-course"),
     path("courses/<str:course_id>/delete/", course_delete_view, name="delete-course"),
-    path(
-        "courses/<str:course_id>/activity/",
-        course_activity_view,
-        name="course-activity",
-    ),
-    path(
-        "courses/<int:courses_author_id>/", courses_by_user_view, name="courses-by-user"
-    ),
-    path("courses/permitted/", courses_by_others_view, name="courses-by-others"),
+    path("courses/<str:course_id>/activity/", course_activity_view, name="course-activity"),
+    path("courses/<int:courses_author_id>/", courses_by_user_view, name="courses-by-user"),
     # Performances
     path("performances/", performances_list_view, name="performed-playlists"),
-    path(
-        "performances/<int:other_id>/",
-        performances_list_view,
-        name="performances-by-user",
-    ),
-    path(
-        "playlist-performance/<int:performance_id>",
-        playlist_performance_view,
-        name="playlist-performance",
-    ),
-    # performance by user of a playlist in the context of a course
+    path("performances/<int:other_id>/", performances_list_view, name="performances-by-user"),
+    path("playlist-performance/<int:performance_id>", playlist_performance_view, name="playlist-performance"),
+        # performance by user of a playlist in the context of a course
     # Connections
     path("connections/", connections_view, name="connections"),
-    path(
-        "connections/toggle-content-permit/<int:other_id>/",
-        toggle_content_permit_view,
-        name="toggle-content-permit",
-    ),
-    path(
-        "connections/toggle-performance-permit/<int:other_id>/",
-        toggle_performance_permit_view,
-        name="toggle-performance-permit",
-    ),
-    path(
-        "connections/toggle-connection-pin/<int:other_id>/",
-        toggle_connection_pin_view,
-        name="toggle-connection-pin",
-    ),
-    path(
-        "connections/toggle-connection-pin/<int:other_id>/confirm/",
-        toggle_connection_pin_with_confirmation,
-        name="toggle-connection-pin-with-confirmation",
-    ),
+    path("courses-by-other-users/", courses_by_others_view, name="courses-by-others"),
+    path("connections/toggle-content-permit/<int:other_id>/", toggle_content_permit_view, name="toggle-content-permit"),
+    path("connections/toggle-performance-permit/<int:other_id>/", toggle_performance_permit_view, name="toggle-performance-permit"),
+    path("connections/toggle-connection-pin/<int:other_id>/", toggle_connection_pin_view, name="toggle-connection-pin"),
+    path("connections/toggle-connection-pin/<int:other_id>/confirm/", toggle_connection_pin_with_confirmation, name="toggle-connection-pin-with-confirmation"),
     # Groups
-    path("connections/groups/", groups_list_view, name="groups-list"),
-    path("connections/groups/add/", group_add_view, name="add-group"),
-    path("connections/groups/<str:group_id>/", group_edit_view, name="edit-group"),
-    path(
-        "connections/groups/<str:group_id>/delete/",
-        group_delete_view,
-        name="delete-group",
-    ),
-    path(
-        "connections/groups/<str:group_id>/remove-member/<int:member_id>/",
-        remove_member,
-        name="remove-group-member",
-    ),
+    path("groups/", groups_list_view, name="groups-list"),
+    path("groups/add/", group_add_view, name="add-group"),
+    path("groups/<str:group_id>/", group_edit_view, name="edit-group"),
+    path("groups/<str:group_id>/delete/", group_delete_view, name="delete-group"),
+    path("groups/<str:group_id>/remove-member/<int:member_id>/", remove_member, name="remove-group-member"),
     # Preferences
     path("preferences/", dashboard_preferences_view, name="preferences"),
     # Import/Export
-    path("exercises/export/", ExerciseExportView.as_view(), name="export-exercises"),
-    path("exercises/import/", ExerciseImportView.as_view(), name="import-exercises"),
-    path("playlists/export/", PlaylistExportView.as_view(), name="export-playlists"),
-    path("playlists/import/", PlaylistImportView.as_view(), name="import-playlists"),
-    path("courses/export/", CourseExportView.as_view(), name="export-courses"),
-    path("courses/import/", CourseImportView.as_view(), name="import-courses"),
+    path("export/exercises/", ExerciseExportView.as_view(), name="export-exercises"),
+    path("import/exercises/", ExerciseImportView.as_view(), name="import-exercises"),
+    path("export/playlists/", PlaylistExportView.as_view(), name="export-playlists"),
+    path("import/playlists/", PlaylistImportView.as_view(), name="import-playlists"),
+    path("export/courses/", CourseExportView.as_view(), name="export-courses"),
+    path("import/courses/", CourseImportView.as_view(), name="import-courses"),
 ]
